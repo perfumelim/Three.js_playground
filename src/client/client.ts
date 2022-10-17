648ad7ed0cf9f98dc90cc1d45ce446df3e1115ad
perfumelim <perfumelim@gmail.com>
2022-10-17 20:42:46 +0900

    Stats Panel

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import Stats from "three/examples/jsm/libs/stats.module";
 
 const scene = new THREE.Scene();
 
@@ -31,19 +32,24 @@ function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
-  // render();
+  render();
   // 위 부분 render는 optional. 왜냐하면 아래 animate에서
   // 60fps로 계속 re-render하고 있기 때문에
-  // resize 이벤트에서도 동일하게 동작한다.
+  // 굳이 쓰지 않아도 resize 이벤트에서도 동일하게 동작한다.
 }
+
+const stats = Stats();
+document.body.appendChild(stats.dom);
 
 function animate() {
   requestAnimationFrame(animate);
-
+  // stats.begin();
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
-
+  // stats.end();
   render();
+
+  stats.update();
 }
 
 function render() {
@@ -51,6 +57,7 @@ function render() {
 }
 
 animate();
+// render();
 // 위에처럼 animate()에서 render를 하는 것도 좋지만
 // 어떤 성능상의 이슈를 위해 무언가가 change될 때 딱 1번만 render하고 싶다면
 // animate()에서 render()를 지우고
