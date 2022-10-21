300f0ca59b4f5fb8c16001fc227455200acc4308
perfumelim <perfumelim@gmail.com>
2022-10-21 17:17:38 +0900

    Try common things I can do with an Object3D Method

--- src/client/client.ts
+++ src/client/client.ts
@@ -4,6 +4,7 @@ import Stats from "three/examples/jsm/libs/stats.module";
 import { GUI } from "dat.gui";
 
 const scene = new THREE.Scene();
+scene.add(new THREE.AxesHelper(5));
 
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -44,10 +45,26 @@ document.body.appendChild(stats.dom);
 
 const gui = new GUI();
 const cubeFolder = gui.addFolder("Cube");
-cubeFolder.add(cube.rotation, "x", 0, Math.PI * 2);
-cubeFolder.add(cube.rotation, "y", 0, Math.PI * 2);
-cubeFolder.add(cube.rotation, "z", 0, Math.PI * 2);
+const cubeRotationFolder = cubeFolder.addFolder("Rotation");
+cubeRotationFolder.add(cube.rotation, "x", 0, Math.PI * 2);
+cubeRotationFolder.add(cube.rotation, "y", 0, Math.PI * 2);
+cubeRotationFolder.add(cube.rotation, "z", 0, Math.PI * 2);
 cubeFolder.open();
+cubeRotationFolder.open();
+const cubePositionFolder = cubeFolder.addFolder("Position");
+cubePositionFolder.add(cube.position, "x", -10, 10, 2);
+cubePositionFolder.add(cube.position, "y", -10, 10, 2);
+cubePositionFolder.add(cube.position, "z", -10, 10, 2);
+cubeFolder.open();
+cubeRotationFolder.open();
+const cubeScaleFolder = cubeFolder.addFolder("Scale");
+cubeScaleFolder.add(cube.scale, "x", -5, 5);
+cubeScaleFolder.add(cube.scale, "y", -5, 5);
+cubeScaleFolder.add(cube.scale, "z", -5, 5);
+cubeFolder.add(cube, "visible");
+cubeFolder.open();
+cubeRotationFolder.open();
+
 const cameraFolder = gui.addFolder("Camera");
 cameraFolder.add(camera.position, "z", 0, 10);
 cameraFolder.open();
@@ -55,8 +72,8 @@ cameraFolder.open();
 function animate() {
   requestAnimationFrame(animate);
   // stats.begin();
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  // cube.rotation.x += 0.01;
+  // cube.rotation.y += 0.01;
   // stats.end();
   render();
 
