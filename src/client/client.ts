bac817f1c0d66dfe86eac942452302c74579a26f
perfumelim <perfumelim@gmail.com>
2022-10-24 14:31:57 +0900

    Experiment the base MeshNormalMaterial

--- src/client/client.ts
+++ src/client/client.ts
@@ -26,21 +26,7 @@ const icosahedronGeometry = new THREE.IcosahedronGeometry(1, 0);
 const planeGeometry = new THREE.PlaneGeometry();
 const torusKnotGeometry = new THREE.TorusKnotGeometry();
 
-const material = new THREE.MeshBasicMaterial(); //{ color: 0x00ff00, wireframe: true })
-
-const texture = new THREE.TextureLoader().load("img/grid.png");
-material.map = texture;
-const envTexture = new THREE.CubeTextureLoader().load([
-  "img/px_50.png",
-  "img/nx_50.png",
-  "img/py_50.png",
-  "img/ny_50.png",
-  "img/pz_50.png",
-  "img/nz_50.png",
-]);
-envTexture.mapping = THREE.CubeReflectionMapping;
-// envTexture.mapping = THREE.CubeRefractionMapping
-material.envMap = envTexture;
+const material = new THREE.MeshNormalMaterial();
 
 const cube = new THREE.Mesh(boxGeometry, material);
 cube.position.x = 5;
@@ -79,11 +65,6 @@ const options = {
     BackSide: THREE.BackSide,
     DoubleSide: THREE.DoubleSide,
   },
-  combine: {
-    MultiplyOperation: THREE.MultiplyOperation,
-    MixOperation: THREE.MixOperation,
-    AddOperation: THREE.AddOperation,
-  },
 };
 
 const gui = new GUI();
@@ -103,26 +84,16 @@ materialFolder
   .onChange(() => updateMaterial());
 materialFolder.open();
 
-const data = {
-  color: material.color.getHex(),
-};
+const meshNormalMaterialFolder = gui.addFolder("THREE.MeshNormalMaterial");
 
-const meshBasicMaterialFolder = gui.addFolder("THREE.MeshBasicMaterial");
-meshBasicMaterialFolder.addColor(data, "color").onChange(() => {
-  material.color.setHex(Number(data.color.toString().replace("#", "0x")));
-});
-meshBasicMaterialFolder.add(material, "wireframe");
-meshBasicMaterialFolder.add(material, "wireframeLinewidth", 0, 10);
-meshBasicMaterialFolder
-  .add(material, "combine", options.combine)
+meshNormalMaterialFolder.add(material, "wireframe");
+meshNormalMaterialFolder
+  .add(material, "flatShading")
   .onChange(() => updateMaterial());
-meshBasicMaterialFolder.add(material, "reflectivity", 0, 1);
-meshBasicMaterialFolder.add(material, "refractionRatio", 0, 1);
-meshBasicMaterialFolder.open();
+meshNormalMaterialFolder.open();
 
 function updateMaterial() {
   material.side = Number(material.side);
-  material.combine = Number(material.combine);
   material.needsUpdate = true;
 }
 
