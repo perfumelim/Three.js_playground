fa94cc710bec7494e94e280926c3b31a89a7d177
perfumelim <perfumelim@gmail.com>
2022-11-11 11:25:50 +0900

    Experiment PLY Loader

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { PLYLoader } from "three/examples/jsm/loaders/PLYLoader";
 import Stats from "three/examples/jsm/libs/stats.module";
 import { GUI } from "dat.gui";
 
@@ -21,61 +22,50 @@ const camera = new THREE.PerspectiveCamera(
 camera.position.z = 7;
 
 const renderer = new THREE.WebGLRenderer();
+renderer.outputEncoding = THREE.sRGBEncoding;
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 new OrbitControls(camera, renderer.domElement);
 
-// const planeGeometry = new THREE.PlaneGeometry(20, 10)//, 360, 180)
-// const plane = new THREE.Mesh(planeGeometry, new THREE.MeshPhongMaterial())
-// plane.rotateX(-Math.PI / 2)
-// //plane.position.y = -1.75
-// scene.add(plane)
+const envTexture = new THREE.CubeTextureLoader().load([
+  "img/px_50.png",
+  "img/nx_50.png",
+  "img/py_50.png",
+  "img/ny_50.png",
+  "img/pz_50.png",
+  "img/nz_50.png",
+]);
+envTexture.mapping = THREE.CubeReflectionMapping;
 
-const torusGeometry = [
-  new THREE.TorusGeometry(),
-  new THREE.TorusGeometry(),
-  new THREE.TorusGeometry(),
-  new THREE.TorusGeometry(),
-  new THREE.TorusGeometry(),
-];
+const material = new THREE.MeshPhysicalMaterial({
+  color: 0x795644,
+  envMap: envTexture,
+  metalness: 0,
+  roughness: 0.25,
+  transparent: false,
+  transmission: 1.0,
+  side: THREE.DoubleSide,
+  clearcoat: 0.0,
+  clearcoatRoughness: 0.25,
+});
 
-const material = [
-  new THREE.MeshBasicMaterial(),
-  new THREE.MeshLambertMaterial(),
-  new THREE.MeshPhongMaterial(),
-  new THREE.MeshPhysicalMaterial({}),
-  new THREE.MeshToonMaterial(),
-];
-
-const torus = [
-  new THREE.Mesh(torusGeometry[0], material[0]),
-  new THREE.Mesh(torusGeometry[1], material[1]),
-  new THREE.Mesh(torusGeometry[2], material[2]),
-  new THREE.Mesh(torusGeometry[3], material[3]),
-  new THREE.Mesh(torusGeometry[4], material[4]),
-];
-
-const texture = new THREE.TextureLoader().load("img/grid.png");
-material[0].map = texture;
-material[1].map = texture;
-material[2].map = texture;
-material[3].map = texture;
-material[4].map = texture;
-
-torus[0].position.x = -8;
-torus[1].position.x = -4;
-torus[2].position.x = 0;
-torus[3].position.x = 4;
-torus[4].position.x = 8;
-
-// light.target.position.set(0, 10, 0);
-
-scene.add(torus[0]);
-scene.add(torus[1]);
-scene.add(torus[2]);
-scene.add(torus[3]);
-scene.add(torus[4]);
+const loader = new PLYLoader();
+loader.load(
+  "models/Troglodytes.ply",
+  function (geometry) {
+    geometry.computeVertexNormals();
+    const mesh = new THREE.Mesh(geometry, material);
+    mesh.rotateX(-Math.PI / 2);
+    scene.add(mesh);
+  },
+  (xhr) => {
+    console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+  },
+  (error) => {
+    console.log(error);
+  }
+);
 
 window.addEventListener("resize", onWindowResize, false);
 function onWindowResize() {
@@ -106,26 +96,10 @@ directionalLightFolder.add(light.position, "y", -100, 100, 0.01);
 directionalLightFolder.add(light.position, "z", -100, 100, 0.01);
 directionalLightFolder.open();
 
-const meshesFolder = gui.addFolder("Meshes");
-meshesFolder.add(data, "mapsEnabled").onChange(() => {
-  material.forEach((m) => {
-    if (data.mapsEnabled) {
-      m.map = texture;
-    } else {
-      m.map = null;
-    }
-    m.needsUpdate = true;
-  });
-});
-
 function animate() {
   requestAnimationFrame(animate);
 
   //helper.update()
-
-  torus.forEach((t) => {
-    t.rotation.y += 0.01;
-  });
 
   render();
 
